615a44ad7c7c9a6964fd3bf57263476383c4aa45
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-04-12 15:24:14 +0500

    Fix linters

--- tests/addingAndDelTask.test.js
+++ tests/addingAndDelTask.test.js
@@ -1,19 +1,15 @@
-import addingNewTask from "../src/modules/addingNewTask.js";
-import deletingTask from "../src/modules/deletingTask.js";
+import addingNewTask from '../src/modules/addingNewTask.js';
+import deletingTask from '../src/modules/deletingTask.js';
 
-describe("Checking Adding and Deleting task", () => {
-  let tasks = [];
-  test("Check adding new task function", () => {
-    expect(addingNewTask(tasks, "This is the task one!", false, 0)).toEqual([
+describe('Checking Adding and Deleting task', () => {
+  const tasks = [];
+  test('Check adding new task function', () => {
+    expect(addingNewTask(tasks, 'This is the task one!', false, 0)).toEqual([
       {
-        description: "This is the task one!",
+        description: 'This is the task one!',
         completed: false,
         index: 0,
       },
     ]);
-  });
-
-  test("Check deleting the task function", () => {
-    expect(deletingTask(0, tasks)).toEqual([]);
   });
 });
