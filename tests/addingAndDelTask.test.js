32cd9700165b23cadd74b9292298c2d627743b54
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-04-13 07:59:29 +0200

    Add some tests

--- tests/addingAndDelTask.test.js
+++ tests/addingAndDelTask.test.js
@@ -2,13 +2,18 @@ import addingNewTask from '../src/modules/addingNewTask.js';
 import deletingTask from '../src/modules/deletingTask.js';
 
 describe('Checking Adding and Deleting task', () => {
+
   test('Check adding new task function', () => {
-    const tasksArr = [{ description: 'task1', completed: false, index: 0 }];
+
+    const tasksArr = [{ description: "task1", completed: false, index: 0 },];
     const expected = [
-      { description: 'task1', completed: false, index: 0 },
-      { description: 'task2', completed: false, index: 1 },
+      { description: "task1", completed: false, index: 0 },
+      { description: "task2", completed: false, index: 1 }
     ];
+
 
     expect(addingNewTask(tasksArr, 'task2', false, 1)).toEqual(expected);
   });
+
+
 });
